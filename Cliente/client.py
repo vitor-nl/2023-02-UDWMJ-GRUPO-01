cccad79f65f1082274ccc4760f037dbe48ff7259
Leonardo Pires - 12922114719 <12922114719@ulife.com.br>
2023-10-06 21:03:05 -0300

    cliente

---
+++ Cliente/client.py
@@ -0,0 +1,8 @@
+class Client:
+  def __init__(self, name: str, lastname: str, address: str, phone: int, gender:str, email: str):
+    self.name = name
+    self.ultimonname = lastname
+    self.address = address
+    self.phone = phone
+    self.gender = gender
+    self.email = email
---
+++ Cliente/network.py
@@ -0,0 +1,5 @@
+class Social:
+  def __init__(self, nameSocial: str, description: str):
+    self.nameSocial = nameSocial
+    self.description = description
+    
