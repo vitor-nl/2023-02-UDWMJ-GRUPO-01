1a54ce60d6c1478556188ca55167b14d261f7fce
LeonardoPires2 <leonardo.carvalho.pires@gmail.com>
2023-10-09 21:07:06 -0300

    up

--- Cliente/category.py
+++ Cliente/category.py
@@ -1,5 +1,5 @@
 class Category:
-  def __init__(self, name, description, date_fabrication, is_active):
+  def __init__(self, name, description):
     self.id = id
     self.name = name
     self.description = description
