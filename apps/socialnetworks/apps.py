c526fe9ede51b563db439461fe7d48b2bbbf2770
Mariana Goncalves <mari.goncalves004@gmail.com>
2023-11-07 22:50:23 -0300

    atributo verbose_name adicionado em apps.py

--- apps/socialnetworks/apps.py
+++ apps/socialnetworks/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SocialnetworksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'socialnetworks'
+    verbose_name = 'Redes Sociais'
