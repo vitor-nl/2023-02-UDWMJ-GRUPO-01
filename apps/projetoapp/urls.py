a0f171bab4f5ab7776c9c5b27b059508d15a89ee
Mariana Goncalves <mari.goncalves004@gmail.com>
2023-11-07 22:47:43 -0300

    criação da estrutura básica da classe socialnetworks

--- apps/projetoapp/urls.py
+++ apps/projetoapp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('categorias/', include('categories.urls', namespace='categories')),
-    path('produtos/', include('products.urls', namespace='products')),
+    path('', include('products.urls', namespace='products')),
 ]
