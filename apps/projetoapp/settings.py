51c66412e7b476580b4168507bfc1bf278362925
Mariana Goncalves <mari.goncalves004@gmail.com>
2023-11-09 08:51:18 -0300

    classe adicionada ao settings.py

--- apps/projetoapp/settings.py
+++ apps/projetoapp/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'categories.apps.CategoriesConfig',
     'products.apps.ProductsConfig',
     'socialnetworks.apps.SocialnetworksConfig',
+    'clients.apps.ClientsConfig',
 ]
 
 MIDDLEWARE = [
