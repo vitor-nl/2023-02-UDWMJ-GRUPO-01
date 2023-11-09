3f82b6a26f873d55f8841adb8cbfafe4047b076b
Mariana Goncalves <mari.goncalves004@gmail.com>
2023-11-09 16:25:00 -0300

    View criadas e migração concluída

--- apps/clients/views.py
+++ apps/clients/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Client, ClientSocialnetwork
+from rest_framework import viewsets
+from .serializer import ClientSerializer, ClientSocialnetworkSerializer
+
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer  
+
+class ClientSocialnetworkViewSet(viewsets.ModelViewSet):
+    queryset = ClientSocialnetwork.objects.all()
+    serializer_class = ClientSocialnetworkSerializer
