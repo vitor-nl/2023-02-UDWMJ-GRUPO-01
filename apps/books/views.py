91d9fb722ce730a602f257d3058105e3aed7888b
Leonardo Carvalho Pires <leonardo.carvalho.pires@gmail.com>
2023-12-06 20:27:49 -0300

    main

--- apps/books/views.py
+++ apps/books/views.py
@@ -1,19 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.shortcuts import render
 from .models import Books
-
-
-# Create your views here.
-
 
 def home(request):
     books = Books.objects.all()
     return render(request, "core/index.html", {"books": books})
-
-
 
 def salvar(request):
     if request.method == "POST":
@@ -22,8 +12,6 @@ def salvar(request):
         sinopse = request.POST.get("sinopse")
         status = request.POST.get("status")
         photo = request.FILES.get("photo")
-    
-
         book = Books(name=name, author=author, sinopse=sinopse, status= status, photo=photo)
 
         book.save()
